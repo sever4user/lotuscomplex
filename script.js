a062228d0e68695363ba5a0d48032d59079f8ad8
sever4user <sever4user@gmail.com>
2026-06-02 00:37:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -32,9 +32,9 @@ const projectsData = [
     year: "2024",
     role: "composer",
     tags: ["game", "horror", "ambient"],
-    desc: "kletka is a popular indie co-op horror set in a universe of samosbor, a russian internet myth about a world made up of one giant, endless panel high-rise buildin.
-      " + "kletka received strong praise from players, reaching 6000+ steam reviews with 90% positive ratings, 400k copies sold and millions of views across youtube
-      " + "i have created two soundtracks for the main menu and one of the game's locations. i'm currently actively collaborating with the developer;",
+    desc: `kletka is a popular indie co-op horror set in a universe of samosbor, a russian internet myth about a world made up of one giant, endless panel high-rise buildin.
+kletka received strong praise from players, reaching 6000+ steam reviews with 90% positive ratings, 400k copies sold and millions of views across youtube
+i have created two soundtracks for the main menu and one of the game's locations. i'm currently actively collaborating with the developer`,
     links: [
       { label: "steam", url: "https://store.steampowered.com/app/1699480/KLET/" }
     ]
