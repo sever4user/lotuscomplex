afa1f471eb8194426518de3b06349e8a0b78f935
sever4user <sever4user@gmail.com>
2026-06-02 19:22:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -24,44 +24,6 @@ const soundtrackCategories = {
   mystuff: { name: "MY STUFF", folder: "soundtracks/mystuff" }
 };
 
-// Projects data
-const projectsData = [
-  {
-    title: "KLETKA",
-    cover: "projects/KLETKA.png",
-    year: "2026",
-    role: "composer",
-    tags: ["game", "horror", "ambient"],
-    desc: "kletka is a popular indie co-op horror set in a universe of samosbor, a russian internet myth about a world made up of one giant, endless panel high-rise buildin.<br /><br />kletka received strong praise from players, reaching 6000+ steam reviews with 90% positive ratings, 400k copies sold and millions of views across youtube.<br /><br />i have created two soundtracks for the main menu and one of the game's locations. i'm currently actively collaborating with the developer",
-    links: [
-      { label: "steam", url: "https://store.steampowered.com/app/1699480/KLET/" }
-    ]
-  },
-  // Add more projects here
-  {
-    title: "AS THE LIGHTS FADE AWAY",
-    cover: "projects/WHEN.png",
-    year: "2026",
-    role: "composer",
-    tags: ["visual novel", "atmospheric", "ambient"],
-    desc: "no description yet",
-    links: [
-      { label: "vk games", url: "https://vkplay.ru/play/game/kogda-gasnut-ogni/" }
-    ]
-  },
-  {
-    title: "PRIVET. ETO NE BOL'NO.",
-    cover: "projects/PRIVET.png",
-    year: "2025",
-    role: "composer",
-    tags: ["visual novel", "atmospheric", "ambient"],
-    desc: "no description yet",
-    links: [
-      { label: "steam", url: "https://store.steampowered.com/app/3913290/PRIVET_ETO_NE_BOLNO/" }
-    ]
-  }  
-];
-
 const ownMusicReleases = [
   {
     title: "sever4user / Sib The Maid - cigarettes after cigarettes",
