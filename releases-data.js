a322188801a528d7c617648a87cebd4de26e6861
sever4user <sever4user@gmail.com>
2026-06-02 19:24:07 +0300

    Update releases-data.js

--- releases-data.js
+++ releases-data.js
@@ -1,1 +1,7 @@
-
+const ownMusicReleases = [
+  {
+    title: "sever4user / Sib The Maid - cigarettes after cigarettes",
+    cover: "covers/cigarettes after cigarettes.png",
+    streamingUrl: "https://bandlink.com/"
+  }
+];
