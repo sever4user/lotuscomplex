1c106db9d983435ea2e954a56ade73901999cc45
sever4user <sever4user@gmail.com>
2026-06-02 19:29:55 +0300

    Create categories-data.js

---
+++ categories-data.js
@@ -0,0 +1,7 @@
+// Soundtrack categories
+const soundtrackCategories = {
+  kletka: { name: "KLETKA", folder: "soundtracks/kletka" },
+  lights: { name: "AS THE LIGHTS FADE AWAY", folder: "soundtracks/lights" },
+  privet: { name: "PRIVET. ETO NE BOL'NO.", folder: "soundtracks/privet" },
+  mystuff: { name: "MY STUFF", folder: "soundtracks/mystuff" }
+};
