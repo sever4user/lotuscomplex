43422f4743a2a3e8e26ab3589bbeef9d657222e1
sever4user <sever4user@gmail.com>
2026-06-07 00:47:34 +0300

    Update releases-data.js

--- releases-data.js
+++ releases-data.js
@@ -1,12 +1,13 @@
 const ownMusicReleases = [
   {
-    title: "sever4user - 你的最後一個四月",
-    cover: "covers/1.png",
-    streamingUrl: "https://band.link/s4us"
-  },
-  {
     title: "CAT GRAY EYES /w sever4user - asleep?",
     cover: "covers/1.2 dith.png",
     streamingUrl: "https://band.link/asIeep"
+
+  },
+  {
+    title: "sever4user - 你的最後一個四月",
+    cover: "covers/1.png",
+    streamingUrl: "https://band.link/s4us"
   }
 ];
