e3f8206162dfab8b244f509e76963f919277a472
sever4user <sever4user@gmail.com>
2026-06-07 00:54:05 +0300

    Update releases-data.js

--- releases-data.js
+++ releases-data.js
@@ -1,9 +1,28 @@
 const ownMusicReleases = [
   {
+    title: "DJ UNIVXRSEL /w sever4user - nightfall",
+    cover: "covers/4.2 dith.png",
+    streamingUrl: "https://vk.com/music/album/-2000839458_19839458_6b5ffcffe3fdf52c1d"
+  },
+  {
+    title: "sever4user - ki wa rantandesu /w heoski",
+    cover: "covers/4.png",
+    streamingUrl: "https://band.link/kiwarantandesu"
+  },
+  {
+    title: "sever4user - acid /w heoski",
+    cover: "covers/3.png",
+    streamingUrl: "https://band.link/severheoski_acid"
+  },
+  {
+    title: "sever4user - 我不明白我腦子裡在想什麼 /w heoski",
+    cover: "covers/2.png",
+    streamingUrl: "https://band.link/idont"
+  },
+  {
     title: "CAT GRAY EYES /w sever4user - asleep?",
     cover: "covers/1.2 dith.png",
     streamingUrl: "https://band.link/asIeep"
-
   },
   {
     title: "sever4user - 你的最後一個四月",
