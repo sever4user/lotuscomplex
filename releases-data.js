3d01b6b3ecd44992085dc37ee706e1efdc4def8d
sever4user <sever4user@gmail.com>
2026-06-07 12:41:40 +0300

    Update releases-data.js

--- releases-data.js
+++ releases-data.js
@@ -1,6 +1,86 @@
 const ownMusicReleases = [
   {
-    title: "DJ UNIVXRSEL /w sever4user - nightfall",
+    title: "[FunctioN] — 1.56 Gkm /w sever4user",
+    cover: "covers/14.png",
+    streamingUrl: "https://band.link/ef1W6"
+  },   
+  {
+    title: "Original Soundtrack of KLETKA (Vol. 3)",
+    cover: "covers/kletkaost2.png",
+    streamingUrl: "https://band.link/kletka_ost_3"
+  },    
+  {
+    title: "Original Soundtrack of KLETKA (Vol. 1)",
+    cover: "covers/kletkaost1.png",
+    streamingUrl: "https://band.link/kletka_ost_1"
+  },    
+  {
+    title: "sever4user - where memories change shape, mossed over /w r1naq",
+    cover: "covers/13.png",
+    streamingUrl: "https://band.link/wherememorieschange"
+  },     
+  {
+    title: "KLETKA - ORIGINAL SOUNDTRACK (Cassette Mix)",
+    cover: "covers/kletkacassette.png",
+    streamingUrl: "https://www.youtube.com/watch?v=FkekGW28JiM"
+  },    
+  {
+    title: "sever4user - voices of etl",
+    cover: "covers/12.png",
+    streamingUrl: "https://band.link/voicesofetl"
+  },    
+  {
+    title: "eikonn, mirror2heaven - cold as ice, leaving my blood on the snow /w sever4user",
+    cover: "covers/11.png",
+    streamingUrl: "https://link.min.eco/coldasicepresave"
+  },     
+  {
+    title: "sever4user - 99th floor melancholy /w Alessandro Povkh, Denis Rakovica",
+    cover: "covers/10.png",
+    streamingUrl: "https://band.link/99thfloor"
+  },   
+  {
+    title: "P.E.N.B. (Original Game Soundtrack from PRIVET. ETO NE BOL'NO.)",
+    cover: "covers/9.2.png",
+    streamingUrl: "https://areyouluvme.band.link/penb"
+  },    
+  {
+    title: "sever4user - angel club (from DATAMOSH Vol.1)",
+    cover: "covers/9.1.png",
+    streamingUrl: "https://bnd.lc/datamosh_vol1"
+  },  
+  {
+    title: "sever4user - cigarettes after cigarettes /w Sib The Maid",
+    cover: "covers/5.png",
+    streamingUrl: "https://band.link/oboretamichi"
+  },  
+  {
+    title: "sever4user - take me to the stratosphere /w white background, r1naq",
+    cover: "covers/8.png",
+    streamingUrl: "https://band.link/tothestratosphere"
+  },  
+  {
+    title: "sever4user - city of loneliness /w white background",
+    cover: "covers/7.2 dith.png",
+    streamingUrl: "https://band.link/ctflnlnss"
+  },
+  {
+    title: "sever4user - you're disappearing again /w r1naq ",
+    cover: "covers/7.png",
+    streamingUrl: "https://band.link/youredisappearing"
+  },
+  {
+    title: "мертвая милая icant - Попытка реинкарнации. /w sever4user",
+    cover: "covers/6.png",
+    streamingUrl: "https://band.link/attempts"
+  },
+  {
+    title: "sever4user - oboreta michi",
+    cover: "covers/5.png",
+    streamingUrl: "https://band.link/oboretamichi"
+  },
+  {
+    title: "DJ UNIVXRSEL - nightfall /w sever4user",
     cover: "covers/4.2 dith.png",
     streamingUrl: "https://vk.com/music/album/-2000839458_19839458_6b5ffcffe3fdf52c1d"
   },
@@ -20,7 +100,7 @@ const ownMusicReleases = [
     streamingUrl: "https://band.link/idont"
   },
   {
-    title: "CAT GRAY EYES /w sever4user - asleep?",
+    title: "CAT GRAY EYES - asleep? /w sever4user",
     cover: "covers/1.2 dith.png",
     streamingUrl: "https://band.link/asIeep"
   },
