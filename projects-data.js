a88a31bc058cbe20f5436d14f91ed76804894107
sever4user <sever4user@gmail.com>
2026-06-11 18:59:47 +0300

    Update projects-data.js

--- projects-data.js
+++ projects-data.js
@@ -6,7 +6,7 @@ const projectsData = [
     year: "2026",
     role: "composer",
     tags: ["game", "horror", "ambient"],
-    desc: "kletka is a popular indie co-op horror set in a universe of samosbor, a russian internet myth about a world made up of one giant, endless panel high-rise buildin.<br /><br />kletka received strong praise from players, reaching 6000+ steam reviews with 90% positive ratings, 400k copies sold and millions of views across youtube.<br /><br />i have created two soundtracks for the main menu and one of the game's locations. i'm currently actively collaborating with the developer",
+    desc: "kletka received strong praise from players, reaching 6000+ steam reviews with 90% positive ratings, 400k copies sold and millions of views across youtube.<br /><br />i created two soundtracks for the main menu and one of the game's locations. i'm currently actively collaborating with the developer",
     links: [
       { label: "steam", url: "https://store.steampowered.com/app/1699480/KLET/" }
     ]
@@ -18,7 +18,7 @@ const projectsData = [
     year: "2026",
     role: "composer",
     tags: ["visual novel", "atmospheric", "ambient"],
-    desc: "poignant visual novel set in a ruined world where beauty of life is forgotten.<br /><br />the protagonist builds a mechanical butterfly from scrap to bring a smile to the only soul beside him. a unique narrative about finding meaning in decay through creativity and fragile hope.<br /><br />i created most of the soundtrack, and we're working on new chapters.",
+    desc: "a unique narrative about finding meaning in decline through creativity and fragile hope.<br /><br />i created most of the soundtrack, and we are working on new chapters.",
     links: [
       { label: "vk games", url: "https://vkplay.ru/play/game/kogda-gasnut-ogni/" }
     ]
@@ -28,8 +28,8 @@ const projectsData = [
     cover: "projects/PRIVET.png",
     year: "2025",
     role: "composer",
-    tags: ["visual novel", "atmospheric", "ambient"],
-    desc: "no description yet",
+    tags: ["visual novel", "surreal", "ambient"],
+    desc: "an underground project. discussions about love through surreal images, a deep and unique plot.<br /><br />i created two in-game soundtracks",
     links: [
       { label: "steam", url: "https://store.steampowered.com/app/3913290/PRIVET_ETO_NE_BOLNO/" }
     ]
