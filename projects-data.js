3b5ebc7397838d2a641ca80b5f499bf9723c06d8
sever4user <sever4user@gmail.com>
2026-06-13 14:22:06 +0300

    Update projects-data.js

--- projects-data.js
+++ projects-data.js
@@ -6,7 +6,7 @@ const projectsData = [
     year: "2026",
     role: "composer",
     tags: ["indie game", "co-op horror", "ambient"],
-    desc: "kletka received strong praise from players, reaching 6000+ steam reviews with 90% positive ratings, 400k copies sold and millions of views across youtube.<br /><br />i created two soundtracks for the main menu and one of the game's locations. i'm currently actively collaborating with the developer",
+    desc: "kletka received strong praise from players, reaching 6000+ steam reviews with 90% positive ratings, 400k copies sold and millions of views across youtube.<br /><br />i created two soundtracks for the main menu and one of the game's locations. i'm currently actively collaborating with the developer.",
     links: [
       { label: "steam", url: "https://store.steampowered.com/app/1699480/KLET/" }
     ]
@@ -29,7 +29,7 @@ const projectsData = [
     year: "2025",
     role: "composer",
     tags: ["visual novel", "surreal", "ambient"],
-    desc: "an underground project. discussions about love through surreal images, a deep and unique plot.<br /><br />i created two in-game soundtracks",
+    desc: "an underground project. discussions about love through surreal images, a deep and unique plot.<br /><br />i created two in-game soundtracks.",
     links: [
       { label: "steam", url: "https://store.steampowered.com/app/3913290/PRIVET_ETO_NE_BOLNO/" }
     ]
